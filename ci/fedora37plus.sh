5d6c9edc3b17ccbe132088e7c559956b2b534bb1
Ashray Shah <geektraindev@gmail.com>
2023-11-22 13:02:25 -0600

    Update fedora37plus.sh

--- ci/fedora37plus.sh
+++ ci/fedora37plus.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-dnf install -y rpm-build make gcc gcc-c++ libdrm-devel systemd-rpm-macros glibc-devel glibc-devel.i686 wget git 'dnf-command(builddep)'
+dnf install -y rpm-build make gcc gcc-c++ libdrm-devel systemd-rpm-macros glibc-devel-2.38-10.fc39.i686 glibc32-2.38-10.fc39.x86_64 wget git 'dnf-command(builddep)'
 
 dnf builddep -y --spec ./displaylink.spec
 
